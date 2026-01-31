41dc6f71585e071745d67053ba8c0489c9c8c245
Delta Student <Nikita@LAPTOP.com>
2026-01-31 14:07:50 +0530

    if else tasks

--- Assig3/Character2.java
+++ Assig3/Character2.java
@@ -2,19 +2,18 @@
 
 import java.util.Scanner;
 
-public class Character2{
-	public static void main(String[] args){
+public class Character2 {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter the Character: ");
 		char ch = sc.next().charAt(0);
-		
-		
-		
-		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
-			System.out.println("character is vowel: "+ch);
-		}else{
-			System.out.println("character is consonant: "+ch);
+
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+				|| ch == 'O' || ch == 'U') {
+			System.out.println("character is vowel: " + ch);
+		} else {
+			System.out.println("character is consonant: " + ch);
 		}
 	}
 }
